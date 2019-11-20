0c0144c96a825196fdd0edc26e1412991d2a8674
S van Spelden <sven@vanspelden.nl>
2019-11-20 11:59:15 +0100

    Footer aanpassing

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -97,22 +97,22 @@
         <div class="contact-details py-4">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4 d-flex">
-                        <div class="mr-4"><i class="fas fa-phone-square-alt"></i></div>
+                    <div class="col-md-4 d-md-flex text-center text-lg-left mb-4 mb-md-0">
+                        <div class="mr-md-4"><i class="fas fa-phone-square-alt"></i></div>
                         <div>
                             <span class="font-weight-bold">Telefoonnummer</span><br>
                             +316-10694149
                         </div>
                     </div>
-                    <div class="col-md-4 d-flex">
-                        <div class="mr-4"><i class="fas fa-envelope-square"></i></div>
+                    <div class="col-md-4 d-md-flex text-center text-lg-left mb-4 mb-md-0">
+                        <div class="mr-md-4"><i class="fas fa-envelope-square"></i></div>
                         <div>
                             <span class="font-weight-bold">E-mailadres</span><br>
                             info@floratuin.com
                         </div>
                     </div>
-                    <div class="col-md-4 d-flex">
-                        <div class="mr-4"><i class="fas fa-map-marked-alt"></i></div>
+                    <div class="col-md-4 d-md-flex text-center text-lg-left">
+                        <div class="mr-md-4"><i class="fas fa-map-marked-alt"></i></div>
                         <div>
                             <span class="font-weight-bold">Locatie</span><br>
                             Rijksweg 85, 1787 PK Julianadorp
