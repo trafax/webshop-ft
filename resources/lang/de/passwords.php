d5c26d316f450e30ffcf47176e6dcc047e4925dc
S van Spelden <sven@vanspelden.nl>
2020-03-12 12:43:59 +0100

    Duitse vertaling aangepast

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -14,7 +14,7 @@ return [
 
     'password' => 'Passwörter müssen mindestens 8 Zeichen lang sein und korrekt bestätigt werden.',
     'reset'    => 'Das Passwort wurde zurückgesetzt!',
-    'sent'     => 'Passworterinnerung wurde gesendet!',
+    'sent'     => 'Passworterinnerung wurde gesendet! Überprüfen Sie auch Ihre Spam-Box',
     'token'    => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig oder abgelaufen.',
     'user'     => 'Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.',
 ];
